45e631e2a8e779e1553dd9b3094d1d3d176aaa8e
SYED MUHAMMAD AYAN <ayanjaved791@gmail.com>
2026-09-19 17:20:18 +0500

    Implement addTask function for task management

---
+++ script.js
@@ -0,0 +1,36 @@
+function addTask() {
+
+    let input = document.getElementById("taskInput");
+    let taskText = input.value;
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    let task = document.createElement("span");
+    task.className = "task-text";
+    task.textContent = taskText;
+    task.onclick = function () {
+        task.classList.toggle("completed");
+    };
+
+   
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete-btn";
+
+    deleteButton.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(task);
+    li.appendChild(deleteButton);
+
+    document.getElementById("taskList").appendChild(li);
+
+   
+    input.value = "";
+}
